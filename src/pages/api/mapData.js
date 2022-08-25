02849dec89921618fedcd7b21600267589d6cf50
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-08-25 17:12:39 -0400

    ok

--- src/pages/api/mapData.js
+++ src/pages/api/mapData.js
@@ -12,7 +12,6 @@ export const mapData = (data = [{}]) => {
   const home = section_two_colimns[0];
   const about = section_two_colimns[1];
   const projects = mapProjects(project);
-  console.log(projects);
   return {
     title,
     menu,
