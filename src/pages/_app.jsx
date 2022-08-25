a6bf7ea2d6e59c561e355f882f40d45ffd94f691
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-08-25 11:25:45 -0400

    SEO ADD

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,16 +2,27 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../styles/global-styles';
 import { theme } from '../styles/theme.js';
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Portifólio || Eleomar Doneles</title>
         <meta
           name="description"
-          content="Portifólio pessoal do Eleomar Dorneles"
+          content="Página do Eleomar Dorneles com informações sobre sua formação acadêmica, projetos."
         />
+        <link rel="canonical" href="https://www.eleomardorneles.com.br/" />
+        <meta name="author" content="Eleomar Dorneles" />
+        <meta
+          name="keywords"
+          content="html, css, javascript, node, react, next, programação, portifolio, fullstack, back-end, backend, frontend, front-end, desenvolvedor"
+        />
+        <meta name="robots" content="index,nofollow" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GlobalStyles />
