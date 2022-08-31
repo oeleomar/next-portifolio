df1723e139a88e65be9b821f39b8e31224b841c5
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-08-31 10:59:59 -0400

    Ok

--- src/components/InputComponent/styles.js
+++ src/components/InputComponent/styles.js
@@ -2,10 +2,10 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   ${({ theme, required }) => css`
-  ${required} ? label::after {
-    content: '*';
-    color: red;
-    margin-left: 0.5rem;
-  } : ""
+    > label::after {
+      content: ${required ? "'*'" : "''"};
+      color: red;
+      margin-left: 0.5rem;
+    }
   `}
 `;
