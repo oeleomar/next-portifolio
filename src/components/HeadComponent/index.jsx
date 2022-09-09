5bba49ffdac58fece1cba3a516eb3fe7555126b4
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-09-09 10:55:16 -0400

    Add head content

--- src/components/HeadComponent/index.jsx
+++ src/components/HeadComponent/index.jsx
@@ -19,6 +19,31 @@ export const HeadComponent = () => {
       <meta name="author" content="Eleomar Dorneles" />
       <meta name="robots" content="index,nofollow" />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta
+        name="msapplication-TileImage"
+        content="https://eleomardorneles.com.br/logo-img.png"
+      />
+
+      <meta property="og:site_name" content="Portifólio || Eleomar Doneles" />
+      <meta property="og:title" content="Portifólio || Eleomar Doneles" />
+      <meta
+        property="og:description"
+        content="Página do Eleomar Dorneles com informações sobre sua formação acadêmica e projetos."
+      />
+
+      <meta
+        property="og:image"
+        content="https://eleomardorneles.com.br/logo-img.png"
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:image:type" content="image/png" />
+
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="298" />
+
+      <meta property="og:url" content="https://eleomardorneles.com.br" />
     </Head>
   );
 };
