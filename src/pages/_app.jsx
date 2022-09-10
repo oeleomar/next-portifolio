54b2064f53790550ce7ff281e41c60a11b6c68ab
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-09-10 08:17:56 -0400

    Head bug fixing.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,10 +2,12 @@ import { ThemeProvider } from 'styled-components';
 import P from 'prop-types';
 import { GlobalStyles } from '../styles/global-styles';
 import { theme } from '../styles/theme';
+import { HeadComponent } from '../components/HeadComponent';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <HeadComponent />
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
